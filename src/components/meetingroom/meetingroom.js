4d1b9403278bdbd0d6f7f7647e786f3f34df9113
김현주 <hyeonju926@gmail.com>
2023-12-17 12:49:36 +0900

    회의실 이벤트 중복 확인
    회의실 이벤트 중복 확인

--- src/components/meetingroom/meetingroom.js
+++ src/components/meetingroom/meetingroom.js
@@ -64,200 +64,215 @@ function Meetingroom() {
     }
   };
 
-    const [forceRerender, setForceRerender] = useState(false);
-    
-    //추가  
-    const handleDateSelect = async (arg) => {
-        if (roomId === '0') {
-            alert('본 방은 읽기 전용입니다.');
-            window.location.reload();
+  const [forceRerender, setForceRerender] = useState(false);
+
+  const handleSelectOverlap = (event) => {
+    // 선택한 시간
+    return !events.some((calEvent) =>
+      calEvent.overlap &&
+      calEvent.start < event.end &&
+      calEvent.end > event.start
+    );
+  };
+
+  const handleEventOverlap = (stillEvent, movingEvent) => {
+    // 이동 이벤트
+    return !events.some((calEvent) =>
+      calEvent.overlap &&
+      calEvent.start < movingEvent.event.end &&
+      calEvent.end > movingEvent.event.start
+    );
+  };
+
+  // 이벤트 추가  
+  const handleDateSelect = async (arg) => {
+    if (roomId === '0') {
+      alert('본 방은 읽기 전용입니다.');
+      window.location.reload();
+    } else {
+      const title = window.prompt('회의 일정을 입력해주세요.');
+      if (title) {
+        const event = {
+          title,
+          start: arg.start.getTime(),
+          end: arg.end.getTime(),
+          roomId,
+        };
+
+        const overlapping = events.some((calEvent) =>
+          (calEvent.start < event.end && calEvent.end > event.start) &&
+          (calEvent.extendedProps.roomId === event.roomId)
+        );
+
+        if (overlapping) {
+          alert('이미 이벤트가 있는 시간에는 중복을 할 수 없습니다.');
         } else {
-            const title = window.prompt('회의 일정을 입력해주세요.');
-            if (title) {
-                const event = {
-                    title,
-                    start: arg.start.getTime(),
-                    end: arg.end.getTime(),
-                    roomId,
-                };
+          try {
+            const response = await axios.post(`http://localhost:${myPort}/auth/meetingroom/${roomId}`, [event], {
+              headers: { Authorization: token },
+            });
 
-                const overlapping = events.some((calEvent) =>
-                    (calEvent.start < event.end && calEvent.end > event.start) &&
-                    (calEvent.extendedProps.roomId === event.roomId)
-                );
-
-                if (overlapping) {
-                    alert('이미 이벤트가 있는 시간에는 중복을 할 수 없습니다.');
-                } else {
-                    try {
-                        const response = await axios.post(`http://localhost:${myPort}/auth/meetingroom/${roomId}`, [event], {
-                            headers: { Authorization: token },
-                        });
-
-                        setEvents(prevEvents => [...prevEvents, {
-                            id: response.data.cal_Id,
-                            title: response.data.title,
-                            start: response.data.start,
-                            end: response.data.end,
-                            extendedProps: {
-                                roomId: response.data.roomId,
-                            },
-                        }]);
-                        setForceRerender(prevValue => !prevValue);
-                      } catch (error) {
-                        console.error('이벤트 저장 오류', error);
-                    }
-                    loadEvents();
-                }
-            }
+            setEvents(prevEvents => [...prevEvents, {
+              id: response.data.cal_Id,
+              title: response.data.title,
+              start: response.data.start,
+              end: response.data.end,
+              extendedProps: {
+                roomId: response.data.roomId,
+              },
+            }]);
+            setForceRerender(prevValue => !prevValue);
+          } catch (error) {
+            console.error('이벤트 저장 오류', error);
+          }
+          loadEvents();
         }
-    };
-
-    //drop으로 수정
-    const handleEventDrop = async (info) => {
-        if (roomId === '0') {
-            alert('본 방은 읽기 전용입니다.');
-            window.location.reload();
-        } else {
-            if (window.confirm(`${info.event.title} 일정을 수정하시겠습니까 ?`)) {
-                const events = [{
-                    title: info.event.title,
-                    start: info.event.start.getTime(),
-                    end: info.event.end.getTime(),
-                    roomId: info.event.extendedProps.roomId,
-                }];
-
-                try {
-                    const calId = info.event.id;
-                    const response = await axios.put(`http://localhost:${myPort}/auth/meetingroom/${calId}/${roomId}`, events, {
-                        headers: {
-                            Authorization: token,
-                            'Content-Type': 'application/json',
-                        },
-                    });
-
-                    if (response.data.flag) {
-                        info.revert();
-                    } else {
-                        alert(response.data.msg);
-                    }
-                } catch (error) {
-                    console.error('Error updating event', error);
-                }
-
-                loadEvents();
-            }
-        }
-    };
-
-
-    //Resize수정 
-    const handleEventResize = async (info) => {
-        if (roomId === '0') {
-            alert('본 방은 읽기 전용입니다.');
-            window.location.reload();
-        } else {
-            if (window.confirm(`${info.event.title} 일정을 수정하시겠습니까 ?`)) {
-                const events = [{
-                    title: info.event.title,
-                    start: info.event.start.getTime(),
-                    end: info.event.end.getTime(),
-                    roomId: info.event.extendedProps.roomId,
-                }];
-
-                try {
-                    const calId = info.event.id;
-                    const response = await axios.put(`http://localhost:${myPort}/auth/meetingroom/${calId}/${roomId}`, events, {
-                        headers: {
-                            Authorization: token,
-                            'Content-Type': 'application/json',
-                        },
-                    });
-
-                    if (response.data.flag) {
-                        info.revert();
-                    } else {
-                        alert(response.data.msg);
-                    }
-                } catch (error) {
-                    console.error('Error updating event', error);
-                }
-
-                loadEvents();
-            }
-        }
-    };
-
-    //삭제
-    const handleEventClick = async (info) => {
-        if (roomId === '0') {
-            alert('본 방은 읽기 전용입니다.');
-            info.jsEvent.preventDefault();
-        } else {
-            if (window.confirm(`${info.event.title} 일정을 삭제하시겠습니까 ?`)) {
-                try {
-                    const calId = info.event.id;
-                    const response = await axios.delete(`http://localhost:${myPort}/auth/meetingroom/${calId}`, {
-                        headers: {
-                            Authorization: token,
-                        },
-                    });
-
-                    if (response.data.flag) {
-                        info.event.remove();
-                    } else {
-                        alert(response.data.msg);
-                    }
-                } catch (error) {
-                    console.error('Error deleting event', error);
-                }
-            }
-        }
-    };
-
-
-    return (
-        <div style={{ marginTop: '20px', width: '80%', margin: 'auto' }}>
-          <div layout="fragment" content>
-            <MeetingRoomTabs activeId={roomId} onTabClick={showRoomId} />
-            <div id="wrap">
-              <div style={{ marginTop: '20px' }}>
-                <FullCalendar
-                  key={forceRerender}
-                  plugins={[dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin]}
-                  initialView="timeGridWeek"
-                  headerToolbar={{
-                    left: 'prev,next today',
-                    center: 'title',
-                    right: 'timeGridWeek,timeGridDay,listWeek',
-                  }}
-                  editable={true}
-                  selectable={true}
-                  events={events.map((event) => {
-                    console.log('렌더링할 이벤트:', event);
-                    return {
-                        id: event.cal_Id,
-                        title: event.title,
-                        start: event.start,
-                        end: event.end,
-                        className: `room-${event.roomId}`,
-                        extendedProps: {
-                            roomId: event.roomId,
-                        },
-                    };
-                })}
-    
-                  select={handleDateSelect}
-                  eventClick={handleEventClick}
-                  eventDrop={handleEventDrop}
-                  eventResize={handleEventResize}
-                />
-              </div>
-              <div style={{ clear: 'both' }}></div>
-            </div>
-          </div>
-        </div>
-      );
+      }
     }
-    
-    export default Meetingroom;
+  };
+
+  // drop으로 수정
+  const handleEventDrop = async (info) => {
+    if (roomId === '0') {
+      alert('본 방은 읽기 전용입니다.');
+      window.location.reload();
+    } else {
+      if (window.confirm(`${info.event.title} 일정을 수정하시겠습니까 ?`)) {
+        const events = [{
+          title: info.event.title,
+          start: info.event.start.getTime(),
+          end: info.event.end.getTime(),
+          roomId: info.event.extendedProps.roomId,
+        }];
+
+        try {
+          const calId = info.event.id;
+          const response = await axios.put(`http://localhost:${myPort}/auth/meetingroom/${calId}/${roomId}`, events, {
+            headers: {
+              Authorization: token,
+              'Content-Type': 'application/json',
+            },
+          });
+
+          if (response.data.flag) {
+            info.revert();
+          } else {
+            alert(response.data.msg);
+          }
+        } catch (error) {
+          console.error('Error updating event', error);
+        }
+
+        loadEvents();
+      }
+    }
+  };
+
+  // Resize 수정 
+  const handleEventResize = async (info) => {
+    if (roomId === '0') {
+      alert('본 방은 읽기 전용입니다.');
+      window.location.reload();
+    } else {
+      if (window.confirm(`${info.event.title} 일정을 수정하시겠습니까 ?`)) {
+        const events = [{
+          title: info.event.title,
+          start: info.event.start.getTime(),
+          end: info.event.end.getTime(),
+          roomId: info.event.extendedProps.roomId,
+        }];
+
+        try {
+          const calId = info.event.id;
+          const response = await axios.put(`http://localhost:${myPort}/auth/meetingroom/${calId}/${roomId}`, events, {
+            headers: {
+              Authorization: token,
+              'Content-Type': 'application/json',
+            },
+          });
+
+          if (response.data.flag) {
+            info.revert();
+          } else {
+            alert(response.data.msg);
+          }
+        } catch (error) {
+          console.error('Error updating event', error);
+        }
+
+        loadEvents();
+      }
+    }
+  };
+
+  // 삭제
+  const handleEventClick = async (info) => {
+    if (roomId === '0') {
+      alert('본 방은 읽기 전용입니다.');
+      info.jsEvent.preventDefault();
+    } else {
+      if (window.confirm(`${info.event.title} 일정을 삭제하시겠습니까 ?`)) {
+        try {
+          const calId = info.event.id;
+          const response = await axios.delete(`http://localhost:${myPort}/auth/meetingroom/${calId}`, {
+            headers: {
+              Authorization: token,
+            },
+          });
+
+          if (response.data.flag) {
+            info.event.remove();
+          } else {
+            alert(response.data.msg);
+          }
+        } catch (error) {
+          console.error('Error deleting event', error);
+        }
+      }
+    }
+  };
+
+  return (
+    <div style={{ marginTop: '20px', width: '80%', margin: 'auto' }}>
+      <div layout="fragment" content>
+        <MeetingRoomTabs activeId={roomId} onTabClick={showRoomId} />
+        <div id="wrap">
+          <div style={{ marginTop: '20px' }}>
+            <FullCalendar
+              key={forceRerender}
+              plugins={[dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin]}
+              initialView="timeGridWeek"
+              headerToolbar={{
+                left: 'prev,next today',
+                center: 'title',
+                right: 'timeGridWeek,timeGridDay,listWeek',
+              }}
+              editable={true}
+              selectable={true}
+              events={events.map((event) => ({
+                id: event.cal_Id,
+                title: event.title,
+                start: event.start,
+                end: event.end,
+                className: `room-${event.roomId}`,
+                extendedProps: {
+                  roomId: event.roomId,
+                  overlap: true, 
+                },
+              }))}
+              select={handleDateSelect}
+              eventClick={handleEventClick}
+              eventDrop={handleEventDrop}
+              eventResize={handleEventResize}
+              selectOverlap={handleSelectOverlap} 
+              eventOverlap={handleEventOverlap}
+            />
+          </div>
+          <div style={{ clear: 'both' }}></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Meetingroom;
